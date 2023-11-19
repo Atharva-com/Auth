2f249201f8299ca0d3ce6dda9760a90f2b1929b8
Your Name <you@example.com>
2023-11-19 20:56:11 +0530

    signin route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/Users");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const signup = async (req, res, next) => {
 
     const { username, email, password } = req.body;
@@ -28,4 +29,30 @@ const signup = async (req, res, next) => {
 
 }
 
-module.exports = { signup };
+const signin = async (req, res, next) => { 
+
+    const { email, password } = req.body;
+    try {
+
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            res.status(404).json({ success: false, message: "User not found." })
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            res.status(400).json({ success: false, message: "Invalid Credentials." })
+        }
+        const { password: pass, ...rest } = validUser._doc;
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const expiryDate = new Date(Number(new Date()) + 1 * 24 * 60 * 60 * 1000);
+        res.cookie("access_token", token, { httpOnly: true, expires: expiryDate }).status(200).json({ success: true, message: "Logged in Successfully.", user: rest });
+        
+    } catch (error) {
+
+        next(error);
+        
+    }
+ }
+
+module.exports = { signup, signin };
